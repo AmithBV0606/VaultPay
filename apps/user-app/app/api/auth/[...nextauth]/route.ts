c681bd6f6e5372aba758a1b82c7bca092bb52f6f
AmithBV0606 <amithrao0606@gmail.com>
2024-12-06 13:26:57 +0530

    Completed Authentication testing.

--- apps/user-app/app/api/auth/[...nextauth]/route.ts
+++ apps/user-app/app/api/auth/[...nextauth]/route.ts
@@ -4,3 +4,8 @@ import { authOptions } from "../../../lib/auth"
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
+
+// or
+
+// export const GET = handler;
+// export const POST = handler;
