ccf9bdcce06697321753648c9c12ddb73458e2d4
AmithBV0606 <amithrao0606@gmail.com>
2024-12-08 14:24:06 +0530

    Tested bank-webhook with dummy requests.

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,7 +1,13 @@
+// Creating a dummy bank server
+
+// - Allows VaultPay to generate a token for a payment for a user for some amount.
+// - If user made a successful payment, Bank should hit this webhookUrl for the company.
+
 import express from "express";
 import db from "@repo/db/client";
-
 const app = express();
+
+app.use(express.json());
 
 interface paymentInformationProps {
   token: string;
@@ -9,8 +15,9 @@ interface paymentInformationProps {
   amount: string;
 }
 
-app.post("/hdfcWebhook", async (req, res) => {
+app.post("/hdfcwebhook", async (req, res) => {
   //TODO: Add zod validation here?
+  //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
   const paymentInformation: paymentInformationProps = {
     token: req.body.token,
     userId: req.body.user_identifier,
@@ -19,19 +26,18 @@ app.post("/hdfcWebhook", async (req, res) => {
 
   try {
     await db.$transaction([
-      // Update balance in db :
-      db.balance.update({
+      db.balance.updateMany({
         where: {
           userId: Number(paymentInformation.userId),
         },
         data: {
           amount: {
+            // You can also get this from your DB
             increment: Number(paymentInformation.amount),
           },
         },
       }),
-      //   Update OnRampTransaction :
-      db.onRampTransaction.update({
+      db.onRampTransaction.updateMany({
         where: {
           token: paymentInformation.token,
         },
@@ -41,11 +47,11 @@ app.post("/hdfcWebhook", async (req, res) => {
       }),
     ]);
 
-    res.status(200).json({
+    res.json({
       message: "Captured",
     });
-  } catch (error) {
-    console.log("Error is : ", error);
+  } catch (e) {
+    console.error(e);
     res.status(411).json({
       message: "Error while processing webhook",
     });
--- apps/user-app/components/OnRampTransactions.tsx
+++ apps/user-app/components/OnRampTransactions.tsx
@@ -20,15 +20,17 @@ export const OnRampTransactions = ({
   }
   return (
     <Card title="Recent Transactions">
-      <div className="pt-2">
+      <div className="py-2">
         {transactions.map((t) => (
-          <div className="flex justify-between" key={Math.random()}>
+          <div className="flex justify-between py-2" key={Math.random()}>
             <div>
               <div className="text-sm">Received INR</div>
+
               <div className="text-slate-600 text-xs">
                 {t.time.toDateString()}
               </div>
             </div>
+
             <div className="flex flex-col justify-center">
               + Rs {t.amount / 100}
             </div>
