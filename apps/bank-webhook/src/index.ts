f6527d957938c8986179ffc0596b3f9afc1a9f98
AmithBV0606 <amithrao0606@gmail.com>
2024-12-09 07:28:48 +0530

    Added P2P Transfer feature and updated the DB Schema to support the history of P2P Transfers in future.

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -18,6 +18,7 @@ interface paymentInformationProps {
 app.post("/hdfcwebhook", async (req, res) => {
   //TODO: Add zod validation here?
   //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+  // TODO: Check the status of this onRampTxn. Only allow the webhook to update if the status is "Processing" 
   const paymentInformation: paymentInformationProps = {
     token: req.body.token,
     userId: req.body.user_identifier,
